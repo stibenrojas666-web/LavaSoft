c76219926ca121d04c42ed8b492fd1c1eb944332
Brayan Stiben <stibenrojas666@gmail.com>
2026-09-01 15:00:51 -0500

    Se hizo todos los request para el crud cliente

--- app/Http/Requests/clienteUpdateRequest.php
+++ app/Http/Requests/clienteUpdateRequest.php
@@ -12,7 +12,7 @@ class clienteUpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +23,29 @@ class clienteUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'nombreCliente' => 'required|string|max:100',
+            'apellidoCliente' => 'required|string|max:100',
+            'telefonoCliente' => 'required|string|max:20',
+            'emailCliente' => 'required|string|max:100'
+        ];
+
+    }
+    public function messages(): array
+    {
+        return [
+            'nombreCliente.required' => 'El nombre del cliente es obligatorio.',
+            'nombreCliente.string' => 'El nombre del cliente debe ser una cadena de texto.',
+            'nombreCliente.max' => 'El nombre del cliente no debe exceder los 100 caracteres.',
+            'apellidoCliente.required' => 'El apellido del cliente es obligatorio.',
+            'apellidoCliente.string' => 'El apellido del cliente debe ser una cadena de texto.',
+            'apellidoCliente.max' => 'El apellido del cliente no debe exceder los 100 caracteres.',
+            'telefonoCliente.required' => 'El teléfono del cliente es obligatorio.',
+            'telefonoCliente.string' => 'El teléfono del cliente debe ser una cadena de texto.',
+            'telefonoCliente.max' => 'El teléfono del cliente no debe exceder los 20 caracteres.',
+            'emailCliente.required' => 'El correo electrónico del cliente es obligatorio.',
+            'emailCliente.email' => 'El correo electrónico del cliente debe ser una dirección de correo válida.',
+            'emailCliente.max' => 'El correo electrónico del cliente no debe exceder los 100 caracteres.',
+            'emailCliente.unique' => 'El correo electrónico del cliente ya está en uso.',
         ];
     }
 }
