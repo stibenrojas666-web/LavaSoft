2cc87ca65e3838ebc51df1b4c67f41a4ccbf40e2
Caren Sandoval <sandovalenti41@gmail.com>
2026-09-01 23:51:41 -0500

    Agrega migracion de tabla empleados

---
+++ database/migrations/2026_09_01_030702_create_empleados_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id();
+            $table->string('telefono', 15);
+            $table->string('rh', 5);
+            $table->string('eps', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empleados');
+    }
+};
