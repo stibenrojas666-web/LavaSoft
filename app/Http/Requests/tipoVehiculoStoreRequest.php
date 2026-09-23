2c6f42bf7de4defe4bfffe1e715b23c13551a02d
Caren Sandoval <sandovalenti41@gmail.com>
2026-09-23 15:51:10 -0500

    Agrega validacion para crear tipos de vehiculo

---
+++ app/Http/Requests/tipoVehiculoStoreRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class tipoVehiculoStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nombre' => 'required|string|max:30',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'nombre.required' => 'El campo Nombre es obligatorio.',
+        ];
+    }
+}
