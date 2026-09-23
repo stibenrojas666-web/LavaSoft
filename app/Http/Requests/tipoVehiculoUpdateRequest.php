e3c137cd2307582a62a75fac23096d586f4bf72b
Caren Sandoval <sandovalenti41@gmail.com>
2026-09-23 15:36:13 -0500

    Se Agrego la  validacion para actualizar tipos de vehiculo

---
+++ app/Http/Requests/tipoVehiculoUpdateRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class tipoVehiculoUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nombre' => 'required|string|max:30',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'nombre.string' => 'El campo Nombre debe ser una cadena de texto.',
+        ];
+    }
+
+}
